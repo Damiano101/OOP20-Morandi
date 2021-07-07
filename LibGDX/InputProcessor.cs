a696c1c9997af74f4c67b4ca0bea5382e88a0952
Mek101 <dmorandi101@tuta.io>
2021-07-07 14:50:27 +0200

    Moved dummy LibGDX to separate project

---
+++ LibGDX/InputProcessor.cs
@@ -0,0 +1,23 @@
+﻿using MartyAdventure.LibGDX.Input;
+
+namespace BadLogic.Gdx
+{
+	public interface IInputProcessor
+	{
+		bool KeyDown(Keys key);
+
+		bool KeyUp(Keys key);
+
+		bool KeyTyped(char character);
+
+		bool TouchDown(int screenX, int screenY, int pointer, int button);
+
+		bool TouchUp(int screenX, int screenY, int pointer, int button);
+
+		bool TouchDragged(int screenX, int screenY, int pointer);
+
+		bool MouseMoved(int screenX, int screenY);
+
+		bool Scrolled(float amountX, float amountY);
+	}
+}
